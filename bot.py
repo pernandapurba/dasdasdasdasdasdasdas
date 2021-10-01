ef98b3ee66a708d828f9286ec52c0d1acd1a225b
pernandapurba <91628062+pernandapurba@users.noreply.github.com>
2021-10-01 19:42:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,10 +38,10 @@ else:
     exit()
 
 # join check
-async def check_user(id):
+async def get_user(id):
     ok = True
     try:
-        await BotzHub(GetParticipantRequest(channel='@BotzHub', user_id=id))
+        await BotzHub(GetParticipantRequest(channel='@SFCorpChannel', participant=id))
         ok = True
     except UserNotParticipantError:
         ok = False
@@ -50,31 +50,31 @@ async def check_user(id):
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/start", func=lambda e: e.is_private))
 async def start(event):
     ok = await BotzHub(GetFullUserRequest(event.sender_id))
-    await event.reply(f"Hello {ok.user.first_name}!\nI am a telegraph uploader bot.",
+    await event.reply(f"Hello {ok.user.first_name}!\nI'm SF telegraph Uploader bot.",
                      buttons=[
                          Button.inline("Help", data="help"),
-                         Button.url("Dev", url="https://t.me/BotzHub")
+                         Button.url("More Bots", url="https://t.me/SFCorpChannel/8")
                      ])
 
 @BotzHub.on(events.callbackquery.CallbackQuery(data="help"))
 async def _(event):
     ok = await BotzHub(GetFullUserRequest(event.sender_id))
-    if (await check_user(event.sender_id)) == False:
-        return await event.edit(f"{ok.user.first_name}, please join my channel to use me!", buttons=[Button.url("Join Channel", url="https://t.me/BotzHub")])
-    await event.edit(f"Send me a picture and I will upload it to Telegraph!\n\n~ @BotzHub")
+    if (await get_user(event.sender_id)) == False:
+        return await event.edit(f"{ok.user.first_name}, Tolong join ke Channel untuk menggunakan bot ini!", buttons=[Button.url("Join Channel", url="https://t.me/SFCorpChannel")])
+    await event.edit(f"Kirim saya gambar dan Saya akan upload ke Telegraph!\n\n~[@SF Corp](https://t.me/SFCorpChannel)")
 
 @BotzHub.on(events.NewMessage(incoming=True, func=lambda e: e.is_private and e.media))
 async def uploader(event):
-    if (await check_user(event.sender_id)) is False:
+    if (await get_user(event.sender_id)) is False:
         return
     TMP_DOWNLOAD_DIRECTORY = "./BotzHub/"
     if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)
     pic = event.media
-    ok = await event.reply("`Downloading...`")
+    ok = await event.reply("`Uploading...`")
     downloaded_file_name = await BotzHub.download_media(pic, TMP_DOWNLOAD_DIRECTORY)
     if downloaded_file_name.endswith((".webp")):
-        await ok.edit("`Oh! It's a sticker...\nLemme convert it!!`")
+        await ok.edit("`Oh! Itu sebuah sticker...\nConvret dulu!!`")
         resize_image(downloaded_file_name)
     try:
         media_urls = upload_file(downloaded_file_name)
@@ -84,8 +84,8 @@ async def uploader(event):
         return
     else:
         os.remove(downloaded_file_name)
-        await ok.edit("Uploaded to [Telegraph](https://telegra.ph{})\n\n~ @BotzHub".format(media_urls[0]),
-                    link_preview=True,
+        await ok.edit("Uploaded to **Telegraph**\n\n       Klik link di bawah untuk mengcopy👇🏻👇🏻👇🏻\n```https://telegra.ph{media_urls[0])```\n\n~[@SF Corp](https://t.me/SFCorpChannel)",
+                    link_preview=False,
                     buttons=[
                         Button.url("Link To File", url=f"https://telegra.ph{media_urls[0]}")
                     ])
@@ -95,5 +95,5 @@ def resize_image(image):
     tmp = im.save(image, "PNG")
 
 print("Bot has started.")
-print("Do visit @BotzHub..")
+print("Do visit @SFCorpChannel..")
 BotzHub.run_until_disconnected()
